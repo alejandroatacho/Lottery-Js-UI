f4805f39f8cd1e7ccb05379b9ec318e27f136bd9
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-11 12:30:15 -0400

    update

--- Delete_SASS_Mistakes.sh
+++ Delete_SASS_Mistakes.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Delete folder
+cd src/css
 echo "Deleting my compiler mistakes!"
 sleep 1
 rm -rf mobile.css mobile.css.map 2> /dev/null
 if [ $? -eq 0 ]; then
   echo "Done!"
 else
-  echo "Failed to delete folders or missing folders exist."
+  echo "Failed to delete files or missing files exist."
 fi
 exit
