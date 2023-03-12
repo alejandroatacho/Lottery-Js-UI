0afe40e6d8af7cf06ec99b9722796fa4173f775b
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-11 21:21:04 -0400

    update

--- Delete_SASS_Mistakes.sh
+++ Delete_SASS_Mistakes.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Delete folder
+echo "Hello Cutie _(:3」∠)_ , $(whoami), $(hostname)"
 cd src/style
-echo "Deleting my compiler mistakes!"
-sleep 1
+echo "Deleting my SASS compiler mistakes!You don't deserve to get a salary from Bouke!"
+sleep 3
 rm -rf mobile.css mobile.css.map 2> /dev/null
 if [ $? -eq 0 ]; then
-  echo "Done!"
+  echo "Done!, all your compiler mistakes have been deleted."
+  sleep 2
+  echo "Closing Terminal!"
+  sleep 1
 else
   echo "Failed to delete files or missing files exist."
+  sleep 1
 fi
+open menu.html
 exit
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # lottery_js_ui
+## to-do-list
+<p>Fix Report generator and maybe make it output in excel instead of *.txt</p>
 
-Formula for the Lottery Ui
+![A picture of applesauce](src/images/start_applesauce.jpg)
 
-# <https://www.youtube.com/watch?v=x28LkA88LmQ>
+
+
+### Formula for the Lottery Ui
+<iframe width="560" height="315" src="https://www.youtube.com/embed/[VIDEO_ID]" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
